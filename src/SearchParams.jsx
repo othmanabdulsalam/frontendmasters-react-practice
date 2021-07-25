ea6aae21f03132f0c4e7e2bce319593e44657f57
Othman Abdulsalam <othmanmabdulsalam@gmail.com>
2021-07-25 21:38:50 +0100

    component composition, added results component to handle pets

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,6 +1,7 @@
 // Hook imports
 import { useEffect, useState } from "react";
 import Pet from "./Pet";
+import Results from "./Results";
 import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
@@ -89,11 +90,7 @@ const SearchParams = () =>
                 </label>
                 <button>Submit</button>
             </form>
-            {
-                pets.map((pet) => (
-                    <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
-                ))
-            }
+            <Results pets={pets} />
         </div>
     );
 };
