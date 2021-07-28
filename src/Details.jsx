cc1885a926549648e339ff7c48bc2bd834e428fc
Othman Abdulsalam <othmanmabdulsalam@gmail.com>
2021-07-28 23:32:53 +0100

    messed up badly, but fixed issue and have images on details page

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,13 +1,11 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component
 {
-    constructor()
-    {
-        super();
-        this.state = { loading: true };
-    }
+    state = { loading: true };
+
 
     async componentDidMount()
     {
@@ -15,28 +13,21 @@ class Details extends Component
             `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
         );
         const json = await res.json();
-        this.setState(
-            {
-                loading: false,
-                name: json.pets[0].name,
-
-
-            })
+        this.setState(Object.assign({ loading: false }, json.pets[0]));
     }
 
     render()
     {
-        //console.log(this.state);
-
         if (this.state.loading)
         {
             return <h2>loading … </h2>;
         }
 
-        const { animal, breed, city, state, description, name } = this.state;
+        const { animal, breed, city, state, description, name, images } = this.state;
 
         return (
             <div className="details">
+                <Carousel images={images} />
                 <div>
                     <h1>{name}</h1>
                     <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
